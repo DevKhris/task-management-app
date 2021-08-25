5b92f9a7f27fa5b431342f35ef69089117d233fb
Chris Hernandez <devkhris@hotmail.com>
2021-08-25 01:38:41 -0400

    Added tasks fetch functions with axios and links in App Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
+import axios from 'axios';
 
 // Styles
 import 'bulma/css/bulma.css';
@@ -8,8 +9,57 @@ import './App.css';
 import Navbar from "./Navbar/Navbar.jsx";
 import TaskList from './TaskList/TaskList.jsx';
 
-function App() {
-  return (
+const API = `http://localhost:8000/tasks`;
+
+class App extends Component  {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tasks: [],
+    };
+
+  }
+
+  componentDidMount()
+  {
+    this.fetchIncompletedTasks();
+  }
+
+  async fetchAllTasks()
+  {
+    try {
+      let response = await axios.get(API);
+      this.setState({ tasks: response.data })
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async fetchIncompletedTasks()
+  {
+    try {
+      let response = await axios.get(`${API}/incompleted`);
+      this.setState({ tasks: response.data })
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async fetchCompletedTasks()
+  {
+    try {
+        let response = await axios.get(`${API}/completed`);
+        this.setState({ tasks: response.data })
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  render()
+  {
+    return (
     <div className="App">
       <header className="App-header">
           <Navbar/>
@@ -25,19 +75,51 @@ function App() {
           <div className="columns">
             <div className="column is-one-quarter">
               <div className="box">
-              Show by
+                <div className="card">
+                  <div className="card-title">
+                      Actions
+                  </div>
+                  <a href="#" className="has-text-right button is-info is-rounded mx-1">
+                      Create Task +
+                  </a>
+                  <a href="#" className="has-text-right button is-primary is-rounded mx-1">
+                      Refresh
+                  </a>
+                </div>
+              </div>
+              <div className="box">
+                Filter by
+                  <ul>
+                    <li>
+                      <a className="has-text-secondary"  onClick={() => this.fetchAllTasks()}>
+                        All Tasks
+                      </a>
+                    </li>
+                    <li>
+                      <a className="has-text-secondary"  onClick={() => this.fetchCompletedTasks()}>
+                        Complete Tasks
+                      </a>
+                    </li>
+                    <li>
+                      <a className="has-text-secondary" onClick={() => this.fetchIncompletedTasks()}>
+                        Incomplete Tasks
+                      </a>
+                    </li>
+                  </ul>
               </div>
             </div>
-            <div className="column is-three-fifths">
+            <div className="column is-fifth-quarters">
               <div className="box">
-                <TaskList/>
+                <TaskList tasks={this.state.tasks}/>
               </div>
             </div>
             <div className="column is-one-quarter">
+
               <div className="box">
                  Search
-                <form action="">
-                  <input type="text" placeholder="Search Task by Name"/>
+                <form className="inline" action="">
+                  <input className="input border-secondary is-rounded" type="text" placeholder="Search Task by Name"/>
+                  <input className="button is-secondary border-secondary is-rounded is-fullwidth" type="submit" value="Search"/>
                 </form>
               </div>
             </div>
@@ -46,6 +128,7 @@ function App() {
       </main>
     </div>
   );
+  }
 }
 
 export default App;
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,41 +1,30 @@
 import React, { Component } from "react";
-import axios from 'axios';
-
-const API = `http://localhost:8080/tasks`;
 
 class TaskList extends Component {
 
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			tasks: [],
-			error: null,
-			completed: null,
-		};
-	}
-
-	componentDidMount()
-	{
-		this.fetchTasks();
-	}
-
-	fetchTasks()
-	{
-		axios.get(API)
-			.then((response) => {
-				response.json()
-				console.log(response.data)
-			})
-			.then((data) => this.setState({ tasks: data }))
-			.catch((error) => this.setState({ error}));
-	}
 	render()
 	{
-		const { tasks } = this.state;
+		const tasks = this.props.tasks
 		return(
 			<div>
-			    Task List ({tasks.length})
+				<div>
+			    	<p className="has-text-left">Task List ({tasks.length}) </p>
+			    	<div className="box">
+			    	 	{tasks.map((task, index) => (
+			    	 		<div>
+			    	 			{task.name}
+			    	 			<time>
+			    	 				{task.completed_at}
+			    	 			</time>
+			    	 			<form action="">
+			    	 			</form>
+			    	 			<form action="">
+			    	 			<input className="button is-danger" type="submit" value="Delete"/>
+			    	 			</form>
+			    	 		</div>
+                  		))}
+			    	</div>
+			    </div>
 			</div>
 		);
 	}
